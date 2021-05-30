b5d5eb5eabe70387c3fc55f776289f5e0831fcc7
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-05-30 11:35:55 +0600

    update

--- src/Components/Product/SubProduct/SubProduct.js
+++ src/Components/Product/SubProduct/SubProduct.js
@@ -4,7 +4,7 @@ const SubProduct = (props) => {
     console.log(props);
     const {title, image, id} = props.datum;
     return (
-        <div className="product-card mb-3 p-3">
+        <div className="product-card mb-3 mx-2 p-3">
            <div>
                <img src={image} alt="" className="fluid"  style={{width: '22%'}}/>
            </div>
